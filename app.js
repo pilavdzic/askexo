3942066eab7b355b06ee82b38ee1ee9de112cd04
Korby <mattcorbin@gmail.com>
2023-03-04 00:46:33 +0300

    fixed file path for express

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('../../../home/korby/node_modules/express');
 var app = express();
-app.use(express.static(__dirname + '/public'));
+
+//app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.send('Working now')
+    res.sendFile(__dirname + '/html/index.html')
 });
+
 app.listen(process.env.PORT || 3000)
