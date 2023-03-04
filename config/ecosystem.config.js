44b512c7c85c324681b0719d5da553e00ec805d1
Korby <mattcorbin@gmail.com>
2023-03-04 14:44:28 +0300

    added logger

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -1,26 +1,11 @@
-/*
-module.exports = {
-  apps : [{
-    name: "myapp",
-    script: "/var/www/askexo/app.js",
-	env: {
-        NODE_ENV: 'production',
-        NODE_MODULES_PATH: '../../../home/korby/node_modules',
-		API_KEY: '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'
-      }
-    // other options
-  }]
-}
-*/
-
 module.exports = {
   apps: [{
     name: 'myapp',
     script: '/var/www/askexo/app.js',
-    env_production: {
-      NODE_ENV: 'production',
-      NODE_MODULES_PATH: '/home/korby/node_modules',
-      API_KEY: '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'
+    env: {
+      'NODE_ENV': 'production',
+      'NODE_MODULES_PATH': '/home/korby/node_modules',
+      'API_KEY': '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'
     }
   }]
 }
