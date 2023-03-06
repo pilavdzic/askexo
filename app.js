b69823b77d2b428837cad973125f6a44abefc947
Korby <mattcorbin@gmail.com>
2023-03-06 20:17:38 +0300

    added logo

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); // this is used for parsing the JSON object from POST
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
