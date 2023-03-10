2e6324728393523caf471fcc22396c5147b478f2
Korby <mattcorbin@gmail.com>
2023-03-10 18:59:02 +0300

    first operational model

--- app.js
+++ app.js
@@ -22,5 +22,5 @@ app.post('/btnSubmit', async (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-	console.log(`Server is listening on port ${process.env.PORT || 3000}`);
+	console.log(`Server is now listening on port ${process.env.PORT || 3000}`);
 })
