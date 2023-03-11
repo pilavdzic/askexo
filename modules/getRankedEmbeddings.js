51d14f7c2ebd3551272706c2016b8e63d022e6df
Korby <mattcorbin@gmail.com>
2023-03-12 00:27:31 +0300

    first attempt to fix memory problem

---
+++ modules/getRankedEmbeddings.js
@@ -0,0 +1,140 @@
+const nodeModulesPath = require('./getNodeModulesPath')
+const getQueryEmbedding = require('./getQueryEmbedding').getQueryEmbedding
+const Decimal = require(`${nodeModulesPath}/decimal.js`);
+const { parse } = require(`${nodeModulesPath}/csv-parse`);
+const { createReadStream } = require('fs');
+const dataPath = './data/'
+
+function vectorSimilarity(array1, array2) {
+  if (array1.length !== array2.length) {
+    throw new Error("Arrays must have the same length");
+  }
+  let result = new Decimal(0);
+  for (let i = 0; i < array1.length; i++) {
+    result = result.plus(array1[i].times(array2[i]));
+  }
+  return result;
+}
+
+async function getRankedEmbeddings(txt) {
+  try {
+    let rowNumber = 0;
+    const output = [];
+    const maxOutputRows = 25;
+
+    const qryEmbed = await getQueryEmbedding(txt);
+    const stream = createReadStream(dataPath + 'embeddings.csv').pipe(parse({ delimiter: ',', from_line: 2 }));
+
+    stream.on('data', (row) => {
+      rowNumber += 1;
+      var lineData = row;
+	  const reg = lineData.shift();
+	  const hash = lineData.shift();
+      const similarity = vectorSimilarity(qryEmbed, lineData);
+      if (output.length < maxOutputRows) {
+        output.push([similarity, rowNumber, reg]);
+      } else {
+        const last = output.pop();
+        const toAdd = (similarity > last[0]) ? [similarity, rowNumber, reg] : last;
+        output.push(toAdd);
+      }
+      output.sort((a, b) => b[0] - a[0]);
+    });
+
+    return new Promise((resolve, reject) => {
+      stream.on('end', () => {
+        resolve(output);
+      });
+      stream.on('error', (error) => {
+        reject(error);
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+
+/*
+async function getRankedEmbeddings(txt) {
+  return new Promise((resolve, reject) => {
+    let rowNumber = 0;
+    const output = [];
+    const maxOutputRows = 25;
+
+    const qryEmbed = await getQueryEmbedding(txt);
+    const stream = createReadStream(dataPath + 'embeddings.csv')
+      .pipe(parse({ delimiter: ',', from_line: 2 }));
+
+    stream.on('data', (row) => {
+      rowNumber += 1;
+      var lineData = row;
+	  const reg = lineData.shift();
+	  const hash = lineData.shift();
+      const similarity = vectorSimilarity(qryEmbed, lineData);
+      if (output.length < maxOutputRows) {
+        output.push([similarity, rowNumber, reg]);
+      } else {
+        const last = output.pop();
+        const toAdd = (similarity > last[0]) ? [similarity, rowNumber, reg] : last;
+        output.push(toAdd);
+      }
+      output.sort((a, b) => b[0] - a[0]);
+    });
+
+    stream.on('end', () => {
+      resolve(output);
+    });
+
+    stream.on('error', (error) => {
+      reject(error);
+    });
+  });
+}
+*/
+/*
+
+async function getRankedEmbeddings(txt){
+	var rowNumber = 0;
+	const output = [];
+	const maxOutputRows = 25;
+	try{
+		const qryEmbed = await getQueryEmbedding(txt);
+		fs.createReadStream(dataPath + 'embeddings.csv')
+		.pipe(parse({ delimiter: ",", from_line: 2 }))
+		.on("data", function (row) {
+			rowNumber += 1;
+			var lineData = row;
+			const reg = lineData.shift();
+			const hash = lineData.shift();
+			const similarity = vectorSimilarity(qryEmbed, lineData);
+			if (output.length < maxOutputRows){
+				output.push([similarity, rowNumber, reg]);
+			}
+			else{
+				const last = output.pop();
+				//console.log((similarity > last[0]) ? 'new one ' + similarity + '>' + last[0] : 'old one '+ last[0] + '>' + similarity);
+				const toAdd = (similarity > last[0]) ? [similarity, rowNumber, reg] : last;
+				output.push(toAdd);
+			}
+			output.sort(function(a, b) {
+				return b[0] - a[0];
+			});
+		})
+		.on("end", function () {
+			//console.log("finished");
+			//console.log(output);
+			return output;
+		})
+		.on("error", function (error) {
+			console.log(error.message);
+		});
+		}
+	catch(error){
+		console.error(error);
+		}
+}
+*/
+
+module.exports = getRankedEmbeddings;
--- modules/queryBuilder.js
+++ modules/queryBuilder.js
@@ -1,7 +1,7 @@
 const nodeModulesPath = require('./getNodeModulesPath')
 const {encode, decode} = require(`${nodeModulesPath}/gpt-3-encoder`)
 const csvReader = require('./csvReader')
-const getRankedEmbeddings = require('./vectorCalcs');
+const getRankedEmbeddings = require('./getRankedEmbeddings');
 
 const preface = "Billy is a helpful, serious, and meticulous policy expert, who has a deep understanding of US Government Federal Travel regulations. He clearly understands that he should look for answers first in the most relevant policies (the ADS), and only afterward in the more general ones (the FAM, the FAH, and the FTR). He responds to all questions in as much detail as possible, and given his rather pedantic nature he gives relevant quotes and citations from the regulations with every response. Always eager to provide the most accurate answer he can, Billy has a strong aversion to giving incorrect or misleading guidance, so he's quick to point out any ambiguities or limits to his understanding, and he readily admits when he's not sure of the correct response. Please put yourself in the role of Billy and respond to this question, using these potentially relevant regulations";
 const prefaceTokens = encode(preface).length;
